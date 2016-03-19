0295243947abb8d582c346d1e531ea77fd71f0e3
Frixs <tomkveton@seznam.cz>
2016-03-19 17:21:43 +0000

    Side Menu

--- app/assets/javascripts/charts.js
+++ app/assets/javascripts/charts.js
@@ -1,18 +1,18 @@
 function drawChart(order) {
     var container = document.getElementById('chart_timeline_'.concat(order));
-    var chart = new google.visualization.Timeline(container);
+    var chart     = new google.visualization.Timeline(container);
     var dataTable = new google.visualization.DataTable();
     dataTable.addColumn({ type: 'string', id: 'Role' });
     dataTable.addColumn({ type: 'string', id: 'Name' });
     dataTable.addColumn({ type: 'date', id: 'Start' });
     dataTable.addColumn({ type: 'date', id: 'End' });
     dataTable.addRows([
-        [ 'President', 'George Washington', new Date(1789, 3, 30), new Date(1797, 2, 4) ],
-        [ 'President', 'John Adams', new Date(1797, 2, 4), new Date(1801, 2, 4) ],
-        [ 'President', 'Thomas Jefferson', new Date(1801, 2, 4), new Date(1809, 2, 4) ]]);
+        [ 'President', '', new Date(1789, 3, 30), new Date(1797, 2, 4) ],
+        [ 'President', '', new Date(1801, 2, 4), new Date(1809, 2, 4) ]
+    ]);
 
     var options = {
-        colors: ['#D64F05', '#17A3CD'],
+        colors: ['#D64F05'],
     };
                            
     chart.draw(dataTable, options);
@@ -20,7 +20,7 @@ function drawChart(order) {
 
 function drawCharts(){
     var arr_activities = ["2016,0,0,14,0,0","2016,0,0,15,0,0"];
-    var output_charts = "";
+    var output_charts  = "";
     
     for( var i = 0; i < 5; i++ ){
         $("#content-wrapper").append('<h2>Sekačka</h2><div class="chart-box-1"><div id="chart_timeline_'+i+'"></div></div>');
