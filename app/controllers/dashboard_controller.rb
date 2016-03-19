e84ee1e84ada5b9c34766c7a8a3fe712ae35ea6e
Martin Brom <zed97@seznam.cz>
2016-03-19 16:53:19 +0100

    app data json array to front-end

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,10 @@
 class DashboardController < ApplicationController
   def show
+    # @d = User.first.devices
     @d = User.first.devices
+    @d.each do |d|
+      (@m ||= []).push(d.measurements.as_json())
+    end
+    @d = @d.as_json()
   end
 end
