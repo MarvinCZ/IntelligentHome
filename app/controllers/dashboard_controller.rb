51b0086726ab804ee35a22c4b6040e631480a81c
Martin Brom <zed97@seznam.cz>
2016-03-19 22:55:37 +0100

    device to user assignment

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,10 +2,5 @@ class DashboardController < ApplicationController
   before_filter :authenticate_user!, only: [:show]
   
   def show
-    @d = User.first.devices
-    @d.each do |d|
-      (@m ||= []).push(d.measurements.to_json())
-    end
-    @d = @d.as_json()
   end
 end
