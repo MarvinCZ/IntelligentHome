d03aa307fff8880a08c40093b4d64821bde92d50
Martin Bruna <martin.bruna@netbrick.cz>
2016-03-20 09:04:17 +0100

    finnal fix

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -10,27 +10,6 @@ class UserController < ApplicationController
   end
   
   def device
-    @data = User.find_by_id(current_user.id).devices.to_json
-  end
-  
-  def deviceDelete
-    @id = params[:device][:id]
-    @d = Device.find_by_id(@id)
-    @d.user_id = nil
-    @d.save
-    redirect_to devices_path
-  end
-  
-  def device_register
-    @d = Device.find_by_secret(params[:device => [:secret]].to_i)
-    if @d == null
-      @d = Device.new
-    else
-      @d.secret = params[:device => [:secret]].to_i
-    end
-    @d.name = params[:device => [:name]]
-    @d.user = current_user
-    @d.save
-    redirect_to devices_path
+    @dev = Device.new
   end
 end
